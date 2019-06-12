a036d91aee824bc2f33057ce94d8fd719de43d47
silentsoft <hyesung.lee@silentsoft.org>
2019-06-13 01:39:36 +0900

    add popup handler for explore webview engine
    - before >
      - open with new window through context menu didn't worked.
    
    - after >
      - open with system's default browser.

--- src/main/java/org/silentsoft/actlist/view/explore/ExploreController.java
+++ src/main/java/org/silentsoft/actlist/view/explore/ExploreController.java
@@ -47,6 +47,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 
@@ -61,190 +62,205 @@ public class ExploreController extends AbstractViewerController {
 	@Override
 	public void initialize(Parent viewer, Object... parameters) {
 		new Thread(() -> {
-			{
+			Platform.runLater(() -> {
 				StringBuffer userAgent = new StringBuffer();
-				userAgent.append("Actlist-");
-				
-				userAgent.append(BuildVersion.VERSION);
-				
-				if (SystemUtil.isWindows()) {
-					userAgent.append(" windows-");
-				} else if (SystemUtil.isMac()) {
-					userAgent.append(" macosx-");
-				} else if (SystemUtil.isLinux()) {
-					userAgent.append(" linux-");
-				} else {
-					userAgent.append(" unknown-");
+				{
+					userAgent.append("Actlist-");
+					
+					userAgent.append(BuildVersion.VERSION);
+					
+					if (SystemUtil.isWindows()) {
+						userAgent.append(" windows-");
+					} else if (SystemUtil.isMac()) {
+						userAgent.append(" macosx-");
+					} else if (SystemUtil.isLinux()) {
+						userAgent.append(" linux-");
+					} else {
+						userAgent.append(" unknown-");
+					}
+					userAgent.append(SystemUtil.getOSArchitecture());
+					
+					userAgent.append(" platform-");
+					userAgent.append(SystemUtil.getPlatformArchitecture());
 				}
-				userAgent.append(SystemUtil.getOSArchitecture());
+				webView.getEngine().setUserAgent(userAgent.toString());
 				
-				userAgent.append(" platform-");
-				userAgent.append(SystemUtil.getPlatformArchitecture());
-				
-				Platform.runLater(() -> {
-					webView.getEngine().setUserAgent(userAgent.toString());
+				webView.getEngine().setCreatePopupHandler((popupFeatures) -> {
+					WebEngine webEngine = new WebEngine();
+					webEngine.locationProperty().addListener((observable, oldValue, newValue) -> {
+						if (newValue != null) {
+							if (Desktop.isDesktopSupported()) {
+								try {
+									Desktop.getDesktop().browse(URI.create(newValue));
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							} else {
+								webView.getEngine().load(newValue);
+							}
+						}
+					});
+					return webEngine;
 				});
-			}
-			
-			ChangeListener<Worker.State> stateChangeListener = new ChangeListener<Worker.State>() {
-				@Override
-				public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
-					if (State.SUCCEEDED.equals(newValue)) {
-						loadingBox.setVisible(false);
-						
-				        Document document = webView.getEngine().getDocument();
-				        NodeList anchors = document.getElementsByTagName("a");
-				        for (int i=0, j=anchors.getLength(); i<j; i++) {
-				        	Node anchor = anchors.item(i);
-				        	if (anchor instanceof EventTarget) {
-				        		((EventTarget) anchor).addEventListener("click", new EventListener() {
-									@Override
-									public void handleEvent(Event event) {
-										HTMLAnchorElement anchorElement = (HTMLAnchorElement)event.getCurrentTarget();
-										String className = anchorElement.getClassName();
-										if (className != null) {
-											if (className.contains("actlist-plugin-download")) {
-												String href = anchorElement.getHref();
-											    if (href != null) {
-											    	if (href.toLowerCase().endsWith(".jar")) {
-											    		Consumer<String> changeChildNodeClassName = (value) -> {
-											    			if (anchorElement.getChildNodes().getLength() > 0) {
-												    			Node node = anchorElement.getChildNodes().item(0);
-												    			if (node instanceof HTMLElement) {
-												    				HTMLElement iElement = (HTMLElement) node;
-												    				iElement.setClassName(value);
-												    			}
-												    		}
-											    		};
-											    		
-											    		changeChildNodeClassName.accept("fa fa-spinner fa-pulse");
-											    		anchorElement.setClassName(className.replace("actlist-plugin-download", "actlist-plugin-installing"));
-											    		
-											    		new Thread(() -> {
-											    			AtomicBoolean succeedToAutoInstall = new AtomicBoolean(false);
-											    			AtomicInteger responseStatusCode = new AtomicInteger();
-											    			AtomicReference<String> responseReasonPhrase = new AtomicReference<>();
-											    			AtomicReference<Exception> exception = new AtomicReference<>();
-										    				try {
-								    							RESTfulAPI.doGet(href, (beforeRequest) -> {
-								    								
-								    							}, (afterResponse) -> {
-								    								try {
-								    									StatusLine statusLine = afterResponse.getStatusLine();
-								    									if (statusLine != null) {
-								    										responseStatusCode.set(statusLine.getStatusCode());
-								    										responseReasonPhrase.set(statusLine.getReasonPhrase());
-								    									}
-								    									
-								    									HttpEntity entity = afterResponse.getEntity();
-									    								if (entity != null) {
-									    									InputStream content = entity.getContent();
-									    									
-									    									// create a partial file
-									    									String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-									    									Path partialFilePath = Paths.get(System.getProperty("java.io.tmpdir"), uuid.concat(".partial"));
-									    									if (Files.notExists(partialFilePath.getParent())) {
-										    									Files.createDirectories(partialFilePath.getParent());							    										
-									    									}
-									    									Files.createFile(partialFilePath);
-									    									
-									    									// write into partial file
-									    									OutputStream fileStream = new FileOutputStream(partialFilePath.toString());
-									    									IOUtils.copy(content, fileStream);
-									    									fileStream.close();
-									    									
-									    									// test valid jar or not
-									    									new JarFile(partialFilePath.toString()).close();
-									    									
-									    									// determine jar file name
-									    									String _newPluginFileName = String.valueOf(Paths.get(URI.create(href).getPath()).getFileName());
-									    									if (_newPluginFileName.toLowerCase().endsWith(".jar") == false) {
-									    										_newPluginFileName = _newPluginFileName.concat(".jar");
+				
+				ChangeListener<Worker.State> stateChangeListener = new ChangeListener<Worker.State>() {
+					@Override
+					public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
+						if (State.SUCCEEDED.equals(newValue)) {
+							loadingBox.setVisible(false);
+							
+					        Document document = webView.getEngine().getDocument();
+					        NodeList anchors = document.getElementsByTagName("a");
+					        for (int i=0, j=anchors.getLength(); i<j; i++) {
+					        	Node anchor = anchors.item(i);
+					        	if (anchor instanceof EventTarget) {
+					        		((EventTarget) anchor).addEventListener("click", new EventListener() {
+										@Override
+										public void handleEvent(Event event) {
+											HTMLAnchorElement anchorElement = (HTMLAnchorElement)event.getCurrentTarget();
+											String className = anchorElement.getClassName();
+											if (className != null) {
+												if (className.contains("actlist-plugin-download")) {
+													String href = anchorElement.getHref();
+												    if (href != null) {
+												    	if (href.toLowerCase().endsWith(".jar")) {
+												    		Consumer<String> changeChildNodeClassName = (value) -> {
+												    			if (anchorElement.getChildNodes().getLength() > 0) {
+													    			Node node = anchorElement.getChildNodes().item(0);
+													    			if (node instanceof HTMLElement) {
+													    				HTMLElement iElement = (HTMLElement) node;
+													    				iElement.setClassName(value);
+													    			}
+													    		}
+												    		};
+												    		
+												    		changeChildNodeClassName.accept("fa fa-spinner fa-pulse");
+												    		anchorElement.setClassName(className.replace("actlist-plugin-download", "actlist-plugin-installing"));
+												    		
+												    		new Thread(() -> {
+												    			AtomicBoolean succeedToAutoInstall = new AtomicBoolean(false);
+												    			AtomicInteger responseStatusCode = new AtomicInteger();
+												    			AtomicReference<String> responseReasonPhrase = new AtomicReference<>();
+												    			AtomicReference<Exception> exception = new AtomicReference<>();
+											    				try {
+									    							RESTfulAPI.doGet(href, (beforeRequest) -> {
+									    								
+									    							}, (afterResponse) -> {
+									    								try {
+									    									StatusLine statusLine = afterResponse.getStatusLine();
+									    									if (statusLine != null) {
+									    										responseStatusCode.set(statusLine.getStatusCode());
+									    										responseReasonPhrase.set(statusLine.getReasonPhrase());
 									    									}
 									    									
-									    									// check whether duplicated or not and if so, pick a uuid as a file name
-									    									Path newPluginFilePath = Paths.get(System.getProperty("user.dir"), "plugins", _newPluginFileName);
-									    									if (Files.exists(newPluginFilePath)) {
-									    										newPluginFilePath = Paths.get(System.getProperty("user.dir"), "plugins", uuid.concat(".jar"));
-									    									}
-									    									final String newPluginFileName = String.valueOf(newPluginFilePath.getFileName());
-									    									
-									    									// move the partial file to the plugins directory
-									    									Files.move(partialFilePath, newPluginFilePath);
-									    									
-									    									CountDownLatch latch = new CountDownLatch(1);
-									    									Platform.runLater(() -> {
-									    										try {
-									    											PluginManager.load(newPluginFileName, true);
-											    									
-											    									EventHandler.callEvent(getClass(), BizConst.EVENT_SAVE_PRIORITY_OF_PLUGINS);
-									    										} catch (Exception | Error e) {
-									    											e.printStackTrace();
-									    										} finally {
-									    											latch.countDown();
-									    										}
-									    									});
-									    									latch.await();
-									    									
-									    									succeedToAutoInstall.set(true);
+									    									HttpEntity entity = afterResponse.getEntity();
+										    								if (entity != null) {
+										    									InputStream content = entity.getContent();
+										    									
+										    									// create a partial file
+										    									String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+										    									Path partialFilePath = Paths.get(System.getProperty("java.io.tmpdir"), uuid.concat(".partial"));
+										    									if (Files.notExists(partialFilePath.getParent())) {
+											    									Files.createDirectories(partialFilePath.getParent());							    										
+										    									}
+										    									Files.createFile(partialFilePath);
+										    									
+										    									// write into partial file
+										    									OutputStream fileStream = new FileOutputStream(partialFilePath.toString());
+										    									IOUtils.copy(content, fileStream);
+										    									fileStream.close();
+										    									
+										    									// test valid jar or not
+										    									new JarFile(partialFilePath.toString()).close();
+										    									
+										    									// determine jar file name
+										    									String _newPluginFileName = String.valueOf(Paths.get(URI.create(href).getPath()).getFileName());
+										    									if (_newPluginFileName.toLowerCase().endsWith(".jar") == false) {
+										    										_newPluginFileName = _newPluginFileName.concat(".jar");
+										    									}
+										    									
+										    									// check whether duplicated or not and if so, pick a uuid as a file name
+										    									Path newPluginFilePath = Paths.get(System.getProperty("user.dir"), "plugins", _newPluginFileName);
+										    									if (Files.exists(newPluginFilePath)) {
+										    										newPluginFilePath = Paths.get(System.getProperty("user.dir"), "plugins", uuid.concat(".jar"));
+										    									}
+										    									final String newPluginFileName = String.valueOf(newPluginFilePath.getFileName());
+										    									
+										    									// move the partial file to the plugins directory
+										    									Files.move(partialFilePath, newPluginFilePath);
+										    									
+										    									CountDownLatch latch = new CountDownLatch(1);
+										    									Platform.runLater(() -> {
+										    										try {
+										    											PluginManager.load(newPluginFileName, true);
+												    									
+												    									EventHandler.callEvent(getClass(), BizConst.EVENT_SAVE_PRIORITY_OF_PLUGINS);
+										    										} catch (Exception | Error e) {
+										    											e.printStackTrace();
+										    										} finally {
+										    											latch.countDown();
+										    										}
+										    									});
+										    									latch.await();
+										    									
+										    									succeedToAutoInstall.set(true);
+										    								}
+									    								} catch (Exception e) {
+									    									System.out.println(String.format("Failed to install (%s -> status : %d - %s)", href, responseStatusCode.get(), responseReasonPhrase.get()));
+									    									e.printStackTrace();
+									    									exception.set(e);
 									    								}
-								    								} catch (Exception e) {
-								    									System.out.println(String.format("Failed to install (%s -> status : %d - %s)", href, responseStatusCode.get(), responseReasonPhrase.get()));
-								    									e.printStackTrace();
-								    									exception.set(e);
-								    								}
-								    							});
-								    						} catch (Exception e) {
-								    							e.printStackTrace();
-								    							exception.set(e);
-								    						}
-							    							
-							    							if (succeedToAutoInstall.get()) {
-							    								EventHandler.callEvent(getClass(), BizConst.EVENT_PLAY_NEW_PLUGINS_ALARM);
-							    								
-							    								Platform.runLater(() -> {
-							    									Optional<ButtonType> result = MessageBox.showConfirm(App.getStage(), "Succeed to install", "Do you want to check it out the new plugin ?");
-								    								if (result.isPresent() && result.get() == ButtonType.OK) {
-								    									EventHandler.callEvent(getClass(), BizConst.EVENT_SHOW_PLUGINS_VIEW, false);
-								    								}
-							    								});
-							    							} else {
-							    								MessageBox.showException(App.getStage(), String.format("Failed to install (%d)", responseStatusCode.get()), responseReasonPhrase.get(), exception.get());
-							    							}
-											    			
-											    			Platform.runLater(() -> {
-											    				changeChildNodeClassName.accept("fa fa-download");
-											    				anchorElement.setClassName(className.replace("actlist-plugin-installing", "actlist-plugin-download"));
-											    			});
-											    		}).start();
-												    } else {
-														if (Desktop.isDesktopSupported()) {
-															try {
-																Desktop.getDesktop().browse(URI.create(href));
-															} catch (Exception e) {
-																e.printStackTrace();
+									    							});
+									    						} catch (Exception e) {
+									    							e.printStackTrace();
+									    							exception.set(e);
+									    						}
+								    							
+								    							if (succeedToAutoInstall.get()) {
+								    								EventHandler.callEvent(getClass(), BizConst.EVENT_PLAY_NEW_PLUGINS_ALARM);
+								    								
+								    								Platform.runLater(() -> {
+								    									Optional<ButtonType> result = MessageBox.showConfirm(App.getStage(), "Succeed to install", "Do you want to check it out the new plugin ?");
+									    								if (result.isPresent() && result.get() == ButtonType.OK) {
+									    									EventHandler.callEvent(getClass(), BizConst.EVENT_SHOW_PLUGINS_VIEW, false);
+									    								}
+								    								});
+								    							} else {
+								    								MessageBox.showException(App.getStage(), String.format("Failed to install (%d)", responseStatusCode.get()), responseReasonPhrase.get(), exception.get());
+								    							}
+												    			
+												    			Platform.runLater(() -> {
+												    				changeChildNodeClassName.accept("fa fa-download");
+												    				anchorElement.setClassName(className.replace("actlist-plugin-installing", "actlist-plugin-download"));
+												    			});
+												    		}).start();
+													    } else {
+															if (Desktop.isDesktopSupported()) {
+																try {
+																	Desktop.getDesktop().browse(URI.create(href));
+																} catch (Exception e) {
+																	e.printStackTrace();
+																}
 															}
-														}
+													    }
 												    }
-											    }
-											    
-											    event.preventDefault();
-											} else if (className.contains("actlist-plugin-installing")) {
-												event.preventDefault();
+												    
+												    event.preventDefault();
+												} else if (className.contains("actlist-plugin-installing")) {
+													event.preventDefault();
+												}
 											}
 										}
-									}
-								}, false);
-				        	}
-				        }
-				    } else if (State.FAILED.equals(newValue)) {
-				    	webView.getEngine().getLoadWorker().stateProperty().removeListener(this);
-				    	
-				    	showFailureContent();
-				    }
-				}
-			};
-			Platform.runLater(() -> {
+									}, false);
+					        	}
+					        }
+					    } else if (State.FAILED.equals(newValue)) {
+					    	webView.getEngine().getLoadWorker().stateProperty().removeListener(this);
+					    	
+					    	showFailureContent();
+					    }
+					}
+				};
 				webView.getEngine().getLoadWorker().stateProperty().addListener(stateChangeListener);
 				
 				webView.getEngine().load("http://actlist.silentsoft.org/explore/");
